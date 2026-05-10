68e1e873d8a6f0101fd7a34dfddf2105fdb9f8d1
Samrat Parajuli <code.samzt13@gmail.com>
2026-05-10 22:21:05 +0545

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,91 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { BrowserRouter, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 import Library from "./pages/Library";
-import About from './pages/About';
-import Contact from './pages/Contact';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Start from "./pages/Start";
 import Onboarding from "./components/Onboarding";
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
-import Focus from './pages/Focus';
-import Confess from './pages/Confess';
-import Setting from './pages/Setting';
+import Focus from "./pages/Focus";
+import Confess from "./pages/Confess";
+import Setting from "./pages/Setting";
 import Problems from "./pages/Problems";
 
 export default function App() {
-  return (  
+  return (
     <BrowserRouter>
       <Routes>
+        {/* Public */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/library" element={<Library />} />
+        <Route path="/start" element={<Start />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/start" element={<Start />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/focus" element={<Focus />} />
-        <Route path="/confess" element={<Confess />} />
-        <Route path="/settings" element={<Setting />} />
-        <Route path="/problems" element={<Problems />} />
-        <Route path="*" element={<NotFound />}/>
+        <Route path="/library" element={<Library />} />
+        
+        {/* Protected */}
+        <Route
+          path="/onboarding"
+          element={
+            <ProtectedRoute>
+              <Onboarding />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/focus"
+          element={
+            <ProtectedRoute>
+              <Focus />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/confess"
+          element={
+            <ProtectedRoute>
+              <Confess />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <ProtectedRoute>
+              <Setting />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/problems"
+          element={
+            <ProtectedRoute>
+              <Problems />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
